4a2ee34e74a47ba4c6b3327a0515fa69618c9cba
ayleen99 <ayyleenn1999@gmail.com>
2024-01-20 19:26:51 -0300

    clase3

---
+++ script3.js
@@ -0,0 +1,23 @@
+//OPERADORES
+const variable = 'valor de la variable' // operador de asignacion
+
+const suma = 1 + 3 // + es operador aritmeticode la suma
+const resta = 1 - 3 // - es operador aritmetico de la resta
+const multiplicacion = 1 * 3 // * es operador aritmetico de la multiplicacion
+const division = 1 / 3 // / es operador aritmetico de la division 
+const resto = 5 % 2 // % es el operador aritmetico que nos dara el resto de la division
+const exponente = 3 ** // ** es el operador de la exponenciacion
+
+//let x = 5;
+//let y = 5;
+
+//x++; // aumentador(aumenta 1)
+//y++; //disminuior(disminuye 1)
+
+console.log(suma)
+console.log(resta)
+console.log(multiplicacion)
+console.log(division)
+console.log(exponente)
+console.log(resto)
+console.log(variable)
