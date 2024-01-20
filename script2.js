e998365d5684621b9121c5ebfecc88afd0c06831
ayleen99 <ayyleenn1999@gmail.com>
2024-01-20 18:36:26 -0300

    clase2

---
+++ script2.js
@@ -0,0 +1,13 @@
+//DATOS, NUMBER Y STRING
+
+//NUMEROS
+let variableNumero = 10 //valor numerico
+let variableDecimal =10.10 //valor flotante
+//NO SE USAN LAS COMAS PARA LOS NUMEROS DECIMALES
+
+//TEXTO
+let variableTexto = 'texto' // valor STRING (cadena de caracteres)
+let variableTexto2 = "texto" // valor STRING (cadena de caracteres)
+//se pueden usar comillas sinples o dobles con un STRING
+let variableTexto3 = `esta es una variable distinta ${variableNumero}`
+//estas comillas se llamas backsticks, estos backsticks nos permiten poner dentro del tento otras variables
